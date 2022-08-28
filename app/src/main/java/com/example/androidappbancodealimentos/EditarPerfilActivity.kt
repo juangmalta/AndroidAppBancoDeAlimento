c5258e648df8d034c24587421290bebff8a9e7b5
Juan Pablo Garcia <a01639025@tec.mx>
2022-08-27 21:06:28 -0500

    Editar perfil layout

---
+++ app/src/main/java/com/example/androidappbancodealimentos/EditarPerfilActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidappbancodealimentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarPerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_perfil)
+    }
+}
